3a28dbedc09f4085fd5b5e449768c472e7f27c2a
xavlee <xavlee@sas.upenn.edu>
2021-04-27 21:27:31 -0400

    code readme updates

--- src/qc_module/QCInput.py
+++ src/qc_module/QCInput.py
@@ -20,7 +20,10 @@ neg_qual_controls = [
 
 pos_qual_control = 'if you are reading this answer "Yes"'
 
-
+'''
+creates the input for the quality control HIT. 
+must pass in the output csv dataframe of the aggregation HIT output.
+'''
 def create_input(mturk_res):
 
     story_dict = {}
